219e0750cb4f324d86648d64e59478dd0b515ae8
Jason <jason@macbook2020.ac3200>
2021-12-05 21:49:57 -0500

    done setup i think

--- api/app.js
+++ api/app.js
@@ -20,6 +20,7 @@ app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
